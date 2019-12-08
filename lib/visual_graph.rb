7e97d2cde113f89a1a161781320e318636d593ff
Jozef Randjak <randjakjozef@gmail.com>
2019-12-08 22:57:06 +0100

    ui updates

--- lib/visual_graph.rb
+++ lib/visual_graph.rb
@@ -34,22 +34,12 @@ class VisualGraph
                                   		    #{@bounds[:maxlon]},#{@bounds[:maxlat]}",
                               		    outputorder: :nodesfirst)
 
-    if @id_start != nil && @id_end != nil
-      # append all vertices
-      @visual_vertices.each { |k,v|
-        graph_viz_output.add_nodes( v.id ,
-                                    :shape => 'box',
-                                    :comment => "#{v.lat},#{v.lon}!!",
-                                    :pos => "#{v.y},#{v.x}!")
-      }
-    else
-      # append all vertices
-      @visual_vertices.each { |k,v|
-        graph_viz_output.add_nodes( v.id , :shape => 'point',
-                                    :comment => "#{v.lat},#{v.lon}!!",
-                                    :pos => "#{v.y},#{v.x}!")
-      }
-    end
+    # append all vertices
+    @visual_vertices.each { |k,v|
+      graph_viz_output.add_nodes( v.id , :shape => 'point',
+                                  :comment => "#{v.lat},#{v.lon}!!",
+                                  :pos => "#{v.y},#{v.x}!")
+    }
 
     # append all edges
     @visual_edges.each { |edge|
@@ -80,7 +70,7 @@ class VisualGraph
           graph_viz_output.add_nodes( v.id ,
                                       :shape => 'point',
                                       :color => 'red',
-                                      :width => '0.3',
+                                      :width => '0.2',
                                       :comment => "#{v.lat},#{v.lon}!!",
                                       :pos => "#{v.y},#{v.x}!")
         else
@@ -102,6 +92,48 @@ class VisualGraph
     # append all edges
     @visual_edges.each { |edge|
       graph_viz_output.add_edges( edge.v1.id, edge.v2.id, 'arrowhead' => 'none' )
+    }
+
+    # export to a given format
+    format_sym = export_filename.slice(export_filename.rindex('.')+1,export_filename.size).to_sym
+    graph_viz_output.output( format_sym => export_filename )
+  end
+
+  # Export +self+ into Graphviz file given by +export_filename+.
+  def export_graphviz(export_filename, lat_start, lon_start, lat_end, lon_end)
+    # create GraphViz object from ruby-graphviz package
+    graph_viz_output = GraphViz.new( :G,
+                                     use: :neato,
+                                     truecolor: true,
+                                     inputscale: @scale,
+                                     margin: 0,
+                                     bb: "#{@bounds[:minlon]},#{@bounds[:minlat]},
+                                  		    #{@bounds[:maxlon]},#{@bounds[:maxlat]}",
+                                     outputorder: :nodesfirst)
+
+    # append all vertices
+    @visual_vertices.each { |k,v|
+      graph_viz_output.add_nodes( v.id , :shape => 'point',
+                                  :comment => "#{v.lat},#{v.lon}!!",
+                                  :pos => "#{v.y},#{v.x}!")
+    }
+
+    # append all vertices
+    visual_edges.each { |edge|
+      latStart = lat_start.to_f
+      lonStart = lon_start.to_f
+      latEnd = lat_end.to_f
+      lonEnd = lon_end.to_f
+
+      test1 = edge.v1.lat
+      test2 = edge.v1.lon
+      # 50.0865517
+      # 14.4625145
+      if ((lat_start.to_f == edge.v1.lat and lonStart == edge.v1.lon) or (latEnd == edge.v2.lat and lonEnd == edge.v2.lon))
+        graph_viz_output.add_edges( edge.v1.id, edge.v2.id, 'arrowhead' => 'none', 'color' => 'red', 'penwidth' => '4' )
+      else
+        graph_viz_output.add_edges( edge.v1.id, edge.v2.id, 'arrowhead' => 'none')
+      end
     }
 
     # export to a given format
---
+++ osm2graphviz.rb
@@ -0,0 +1,144 @@
+require_relative 'lib/graph_loader';
+require_relative 'lib/graph_comp_finder';
+require_relative 'process_logger';
+
+# Class representing simple navigation based on OpenStreetMap project
+class OSM2graphviz
+
+  # Creates an instance of navigation. No input file is specified in this moment.
+  def initialize
+    # register
+    @load_cmds_list = ['--load-comp', ]
+    @actions_list = ['--midist']
+
+    @usage_text = <<-END.gsub(/^ {6}/, '')
+	  	Usage:\truby osm_simple_nav.rb <load_command> <input.IN> <action_command> <output.OUT> 
+	  	\tLoad commands: 
+	  	\t\t --load ... load map from file <input.IN>, IN can be ['DOT']
+	  	\tAction commands: 
+	  	\t\t --export ... export graph into file <output.OUT>, OUT can be ['PDF','PNG','DOT']
+    END
+  end
+
+  # Prints text specifying its usage
+  def usage
+    puts @usage_text
+  end
+
+  # Command line handling
+  def process_args
+    # not enough parameters - at least load command, input file and action command must be given
+    if ARGV.length < 4
+      puts "Not enough parameters!"
+      puts usage
+      exit 1
+    end
+
+    # read load command, input file and action command
+    @load_cmd = ARGV.shift
+    unless @load_cmds_list.include?(@load_cmd)
+      puts "Load command not registred!"
+      puts usage
+      exit 1
+    end
+
+    @map_file = ARGV.shift
+    unless File.file?(@map_file)
+      puts "File #{@map_file} does not exist!"
+      puts usage
+      exit 1
+    end
+
+    @operation = ARGV.shift
+    unless @actions_list.include?(@operation)
+      puts "Action command not registred!"
+      puts usage
+      exit 1
+    end
+
+    @lat_start = ARGV.shift
+    @lon_start = ARGV.shift
+    @lat_end = ARGV.shift
+    @lon_end = ARGV.shift
+
+    # load output file
+    @out_file = ARGV.shift
+  end
+
+  # Determine type of file given by +file_name+ as suffix.
+  #
+  # @return [String]
+  def file_type(file_name)
+    return file_name[file_name.rindex(".")+1,file_name.size]
+  end
+
+  # Specify log name to be used to log processing information.
+  def prepare_log
+    ProcessLogger.construct('log/logfile.log')
+  end
+
+  # Load graph from OSM file. This methods loads graph and create +Graph+ as well as +VisualGraph+ instances.
+  def load_graph
+    graph_loader = GraphLoader.new(@map_file, @highway_attributes)
+    @graph, @visual_graph = graph_loader.load_graph()
+  end
+
+  # Load graph from Graphviz file. This methods loads graph and create +Graph+ as well as +VisualGraph+ instances.
+  def import_graph
+    graph_loader = GraphLoader.new(@map_file, @highway_attributes)
+    @graph, @visual_graph = graph_loader.load_graph_viz
+  end
+
+  def load_comp
+    comp_loader = ComponentFinder.new()
+    @graph, @visual_graph = comp_loader.get_comp(@graph, @visual_graph)
+  end
+
+  def run
+    # prepare log and read command line arguments
+    prepare_log
+    process_args
+
+    # load graph - action depends on last suffix
+    #@highway_attributes = ['residential', 'motorway', 'trunk', 'primary', 'secondary', 'tertiary', 'unclassified']
+    @highway_attributes = ['residential', 'motorway', 'trunk', 'primary', 'secondary', 'tertiary', 'unclassified']
+    #@highway_attributes = ['residential']
+    if file_type(@map_file) == "osm" or file_type(@map_file) == "xml" then
+      load_graph
+    elsif file_type(@map_file) == "dot" or file_type(@map_file) == "gv" then
+      import_graph
+    else
+      puts "Imput file type not recognized!"
+      usage
+    end
+
+    if @load_cmd == '--load-comp'
+      load_comp
+    end
+
+    # POZN: Zakreslení bodů proveďte vhodným způsobem, aby jej bylo možné dobře pozorovat
+    # Program bude mít dále schopnost určit pro dvě zadaná místa nejkratší cestu,
+    # pro níž také vypíše informaci o době, za jakou vozidlo cestu ujede (za použití maximální povolené rychlosti).
+    # Navíc je úkolem tuto cestu zakreslit do mapy odlišnou barvou a tloušťkou čáry a tak zvýraznit její průběh ve výstupu.
+
+    # Program bude mít následující rozhraní:
+
+    # ruby osm2graphviz.rb --load-comp <input_map.IN> --midist 50.0865517 14.4625145 50.0898046 14.4611785 <exported_map.OUT>
+    # Výstupem bude tedy mapa, na které budou jednotlivé úseky nejkratší cesty vhodně zvýrazněny - například barvou a tloušťkou čáry.
+
+    # perform the operation
+    case @operation
+    when '--midist'
+        @visual_graph.export_graphviz(@out_file, @lat_start, @lon_start, @lat_end, @lon_end)
+        @visual_graph.print_nodes()
+      return
+    else
+      usage
+      exit 1
+    end
+  end
+end
+
+osm2graphviz = OSM2graphviz.new
+osm2graphviz.run
+
